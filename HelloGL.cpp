e21f276b610dd3ffe1baae28c106e5710fa1f224
Jambient <77830514+Jambient@users.noreply.github.com>
2024-04-16 10:15:44 +0100

    added 3d camera + 3d cube

--- HelloGL.cpp
+++ HelloGL.cpp
@@ -2,13 +2,83 @@
 #include <cmath>
 #include <iostream>
 
+Vertex HelloGL::vertices[] = { 
+	1, 1, 1, -1, 1, 1, -1,-1, 1, // v0-v1-v2 (front)
+	-1,-1, 1, 1,-1, 1, 1, 1, 1, // v2-v3-v0
+
+	1, 1, 1, 1,-1, 1, 1,-1,-1, // v0-v3-v4 (right)
+	1,-1,-1, 1, 1,-1, 1, 1, 1, // v4-v5-v0
+
+	1, 1, 1, 1, 1,-1, -1, 1,-1, // v0-v5-v6 (top)
+	-1, 1,-1, -1, 1, 1, 1, 1, 1, // v6-v1-v0
+
+	-1, 1, 1, -1, 1,-1, -1,-1,-1, // v1-v6-v7 (left)
+	-1,-1,-1, -1,-1, 1, -1, 1, 1, // v7-v2-v1
+
+	-1,-1,-1, 1,-1,-1, 1,-1, 1, // v7-v4-v3 (bottom)
+	1,-1, 1, -1,-1, 1, -1,-1,-1, // v3-v2-v7
+
+	1,-1,-1, -1,-1,-1, -1, 1,-1, // v4-v7-v6 (back)
+	-1, 1,-1, 1, 1,-1, 1,-1,-1 // v6-v5-v4
+};
+
+Color HelloGL::colors[] = { 
+	1, 1, 1, 1, 1, 0, 1, 0, 0, // v0-v1-v2 (front)
+	1, 0, 0, 1, 0, 1, 1, 1, 1, // v2-v3-v0
+
+	1, 1, 1, 1, 0, 1, 0, 0, 1, // v0-v3-v4 (right)
+	0, 0, 1, 0, 1, 1, 1, 1, 1, // v4-v5-v0
+
+	1, 1, 1, 0, 1, 1, 0, 1, 0, // v0-v5-v6 (top)
+	0, 1, 0, 1, 1, 0, 1, 1, 1, // v6-v1-v0
+
+	1, 1, 0, 0, 1, 0, 0, 0, 0, // v1-v6-v7 (left)
+	0, 0, 0, 1, 0, 0, 1, 1, 0, // v7-v2-v1
+
+	0, 0, 0, 0, 0, 1, 1, 0, 1, // v7-v4-v3 (bottom)
+	1, 0, 1, 1, 0, 0, 0, 0, 0, // v3-v2-v7
+
+	0, 0, 1, 0, 0, 0, 0, 1, 0, // v4-v7-v6 (back)
+	0, 1, 0, 0, 1, 1, 0, 0, 1  // v6-v5-v4
+};
+
+Vertex HelloGL::indexedVertices[] = { 
+	1, 1, 1, -1, 1, 1, // v0,v1,
+	-1,-1, 1, 1,-1, 1, // v2,v3
+	1,-1,-1, 1, 1,-1, // v4,v5
+	-1, 1,-1, -1,-1,-1 // v6,v7
+};
+
+Color HelloGL::indexedColors[] = { 
+	1, 1, 1, 1, 1, 0, // v0,v1,
+	1, 0, 0, 1, 0, 1, // v2,v3
+	0, 0, 1, 0, 1, 1, // v4,v5
+	0, 1, 0, 0, 0, 0 //v6,v7
+};
+
+GLushort HelloGL::indices[] = { 
+	0, 1, 2, 2, 3, 0, // front
+	0, 3, 4, 4, 5, 0, // right
+	0, 5, 6, 6, 1, 0, // top
+	1, 6, 7, 7, 2, 1, // left
+	7, 4, 3, 3, 2, 7, // bottom
+	4, 7, 6, 6, 5, 4 // back 
+};
+
 float DegreesToRadians(float degrees) { return degrees * (3.1415926 / 180.0f); }
 
 HelloGL::HelloGL(int argc, char* argv[]) 
 {
+	camera = new Camera();
+	//camera->eye.x = 0.0f; camera->eye.y = 0.0f; camera->eye.z = 1.0f;
+	camera->eye.x = 5.0f; camera->eye.y = 5.0f; camera->eye.z = -5.0f;
+	camera->center.x = 0.0f; camera->center.y = 0.0f; camera->center.z = 0.0f;
+	camera->up.x = 0.0f; camera->up.y = 1.0f; camera->up.z = 0.0f;
+
 	row1Rotation = 0.0f;
 	row2Rotation = 0.0f;
 	row3Rotation = 0.0f;
+	scale = 0.0f;
 
 	GLUTCallbacks::Init(this);
 	glutInit(&argc, argv);
@@ -18,19 +88,35 @@ HelloGL::HelloGL(int argc, char* argv[])
 	glutDisplayFunc(GLUTCallbacks::Display);
 	glutTimerFunc(REFRESHRATE, GLUTCallbacks::Timer, REFRESHRATE);
 	glutKeyboardFunc(GLUTCallbacks::Keyboard);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	// Set the viewport to be the entire window
+	glViewport(0, 0, 800, 800);
+
+	// Set the correct perspective
+	gluPerspective(45, 1, 0, 1000);
+
+	glMatrixMode(GL_MODELVIEW);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
 	glutMainLoop();
 }
 
 HelloGL::~HelloGL(void)
 {
-
+	delete camera;
+	camera = nullptr;
 }
 
 void HelloGL::Display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	DrawTriangleFromAngles(30, 20, 0.3f, std::make_pair(-0.7f, 0.5f), row1Rotation);
+	/*DrawTriangleFromAngles(30, 20, 0.3f, std::make_pair(-0.7f, 0.5f), row1Rotation);
 	DrawTriangleFromAngles(70, 70, 0.3f, std::make_pair(-0.15f, 0.5f), row1Rotation);
 	DrawTriangleFromAngles(60, 60, 0.3f, std::make_pair(0.4f, 0.5f), row1Rotation);
 	DrawTriangleFromAngles(70, 45, 0.3f, std::make_pair(-0.7f, -0.1f), row2Rotation);
@@ -39,7 +125,12 @@ void HelloGL::Display()
 
 	DrawRegularPolygon(std::make_pair(-0.5f, -0.5f), row3Rotation, 0.2f, true, 5);
 	DrawRegularPolygon(std::make_pair(0, -0.5f), row3Rotation, 0.2f, true, 6);
-	DrawRegularPolygon(std::make_pair(0.5f, -0.5f), row3Rotation, 0.2f, true, 7);
+	DrawRegularPolygon(std::make_pair(0.5f, -0.5f), row3Rotation, 0.2f, true, 7);*/
+
+	glPushMatrix();
+	glRotatef(row2Rotation, 1.0f, 1.0f, 1.0f);
+	DrawIndexedCube();
+	glPopMatrix();
 
 	glFlush();
 	glutSwapBuffers();
@@ -47,6 +138,9 @@ void HelloGL::Display()
 
 void HelloGL::Update()
 {
+	glLoadIdentity();
+	gluLookAt(camera->eye.x, camera->eye.y, camera->eye.z, camera->center.x, camera->center.y, camera->center.z, camera->up.x, camera->up.y, camera->up.z);
+
 	//row1Rotation = fmod(row1Rotation + 0.5f, 360.0f);
 	row2Rotation = fmod(row2Rotation + 1.5f, 360.0f);
 	row3Rotation = fmod(row3Rotation - 0.5f, 360.0f);
@@ -129,13 +223,138 @@ void HelloGL::DrawTriangleFromAngles(float angle1, float angle2, float base, vec
 
 void HelloGL::Keyboard(unsigned char key, int x, int y)
 {
-	if (key == 'a')
+	/*if (key == 'a')
 		row1Rotation -= 20.5f;
 	if (key == 'd')
-		row1Rotation += 20.5f;
+		row1Rotation += 20.5f;*/
+
+	if (key == 'w')
+		camera->center.z -= 0.1f;
+	if (key == 's')
+		camera->center.z += 0.1f;
+	if (key == 'a')
+		camera->center.x -= 0.1f;
+	if (key == 'd')
+		camera->center.x += 0.1f;
 }
 
+void HelloGL::DrawCube()
+{
+	glBegin(GL_TRIANGLES);
+	// face v0-v1-v2
+	glColor3f(1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glColor3f(1, 1, 0);
+	glVertex3f(-1, 1, 1);
+	glColor3f(1, 0, 0);
+	glVertex3f(-1, -1, 1);
+	// face v2-v3-v0
+	glColor3f(1, 0, 0);
+	glVertex3f(-1, -1, 1);
+	glColor3f(1, 0, 1);
+	glVertex3f(1, -1, 1);
+	glColor3f(1, 1, 1);
+	glVertex3f(1, 1, 1);
+	// face v0-v3-v4
+	glColor3f(1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glColor3f(1, 0, 1);
+	glVertex3f(1, -1, 1);
+	glColor3f(0, 0, 1);
+	glVertex3f(1, -1, -1);
+	// face v4-v5-v0
+	glColor3f(0, 0, 1);
+	glVertex3f(1, -1, -1);
+	glColor3f(0, 1, 1);
+	glVertex3f(1, 1, -1);
+	glColor3f(1, 1, 1);
+	glVertex3f(1, 1, 1);
+	// face v0-v5-v6
+	glColor3f(1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glColor3f(0, 1, 1);
+	glVertex3f(1, 1, -1);
+	glColor3f(0, 1, 0);
+	glVertex3f(-1, 1, -1);
+	// face v6-v1-v0
+	glColor3f(0, 1, 0);
+	glVertex3f(-1, 1, -1);
+	glColor3f(1, 1, 0);
+	glVertex3f(-1, 1, 1);
+	glColor3f(1, 1, 1);
+	glVertex3f(1, 1, 1);
+	// face v1-v6-v7
+	glColor3f(1, 1, 0);
+	glVertex3f(-1, 1, 1);
+	glColor3f(0, 1, 0);
+	glVertex3f(-1, 1, -1);
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	// face v7-v2-v1
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glColor3f(1, 0, 0);
+	glVertex3f(-1, -1, 1);
+	glColor3f(1, 1, 0);
+	glVertex3f(-1, 1, 1);
+	// face v7-v4-v3
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glColor3f(0, 0, 1);
+	glVertex3f(1, -1, -1);
+	glColor3f(1, 0, 1);
+	glVertex3f(1, -1, 1);
+	// face v3-v2-v7
+	glColor3f(1, 0, 1);
+	glVertex3f(1, -1, 1);
+	glColor3f(1, 0, 0);
+	glVertex3f(-1, -1, 1);
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	// face v4-v7-v6
+	glColor3f(0, 0, 1);
+	glVertex3f(1, -1, -1);
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glColor3f(0, 1, 0);
+	glVertex3f(-1, 1, -1);
+	// face v6-v5-v4
+	glColor3f(0, 1, 0);
+	glVertex3f(-1, 1, -1);
+	glColor3f(0, 1, 1);
+	glVertex3f(1, 1, -1);
+	glColor3f(0, 0, 1);
+	glVertex3f(1, -1, -1);
 
+	glEnd();
+
+}
+
+void HelloGL::DrawCubeArray()
+{
+	glPushMatrix();
+
+	glBegin(GL_TRIANGLES);
+	for (int i = 0; i < 36; i++)
+	{
+		glColor3fv(&colors[i].r);
+		glVertex3fv(&vertices[i].x);
+	}
+	glPopMatrix();
+}
+
+void HelloGL::DrawIndexedCube()
+{
+	glPushMatrix();
+	glBegin(GL_TRIANGLES);
+	for (int i = 0; i < 36; i++)
+	{
+		glColor3fv(&indexedColors[indices[i]].r);
+		glVertex3fv(&indexedVertices[indices[i]].x);
+	}
+	glEnd();
+	glPopMatrix();
+}
 
 int main(int argc, char* argv[]) 
 {
--- HelloGL.h
+++ HelloGL.h
@@ -9,6 +9,26 @@
 #define vector2 std::pair<double, double>
 #define REFRESHRATE 16
 
+struct Vector3
+{
+	float x, y, z;
+};
+
+struct Camera
+{
+	Vector3 eye, center, up;
+};
+
+struct Color
+{
+	GLfloat r, g, b;
+};
+
+struct Vertex
+{
+	GLfloat x, y, z;
+};
+
 class HelloGL
 {
 public:
@@ -17,12 +37,24 @@ public:
 
 	void Update();
 	void Display();
+	void Keyboard(unsigned char key, int x, int y);
+
 	void DrawPolygon();
 	void DrawRegularPolygon(vector2 center, float rotation, float radius, bool filled = true, float sides = 64.0f);
 	void DrawTriangleFromAngles(float angle1, float angle2, float base, vector2 pos, float rotation);
-	void Keyboard(unsigned char key, int x, int y);
+	void DrawCube();
+	void DrawCubeArray();
+	void DrawIndexedCube();
 
 private:
 	float row1Rotation, row2Rotation, row3Rotation;
 	float scale;
+
+	Camera* camera;
+	static Vertex vertices[];
+	static Color colors[];
+	
+	static Vertex indexedVertices[];
+	static Color indexedColors[];
+	static GLushort indices[];
 };
